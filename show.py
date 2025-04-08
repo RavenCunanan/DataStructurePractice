c481110e121feb6db78604c7944a53187b8fc81d
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-04-08 16:51:14 -0700

    Create show.py

---
+++ show.py
@@ -0,0 +1,9 @@
+favorite_shows = ['Breaking Bad', 'Everybody Hates Chris', 'Regular Show']
+
+print(favorite_shows[0])
+print(favorite_shows[1])
+print(favorite_shows[2])
+
+favorite_shows.append("Sopranos")
+
+print(len(favorite_shows))    
